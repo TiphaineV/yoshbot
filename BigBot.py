220fea65fef1f8ec701c84afa7b333fd8b81e499
Tiphaine Viard <tiphaine.viard@gmail.com>
2014-04-17 18:06:41 +0200

    Possibility to add keywords to a !gif command

--- BigBot.py
+++ BigBot.py
@@ -21,14 +21,29 @@ class BigBot(ircbot.SingleServerIRCBot):
 	def getMsg(self, **arg):
 		return Template(random.choice(self.sentences)).safe_substitute(arg)
 
+	def getGif(self, message):
+		tags = ""
+
+		if message[0:4].lower()  == '!gif':
+			contents = message.split(" ")
+			cats = contents[1:]
+			tags = '+'.join(cats)
+
+		json_resp = urllib2.urlopen('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + str(tags)).read()
+                rep = json.loads(json_resp)
+		if len(rep[unicode('data')]) is 0:
+			rep[unicode('data')] = {}
+			rep[unicode('data')][unicode('image_url')] = "i__i"
+
+		return rep
+
 	def on_pubmsg(self, serv, ev):
 		author = irclib.nm_to_n(ev.source())
 		channel = ev.target()
 		message = ev.arguments()[0].lower()
 		
 		if message.find('gif') is not -1:
-			json_resp = urllib2.urlopen('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC').read()
-			rep = json.loads(json_resp)
+			rep = self.getGif(message)
 			serv.privmsg(channel,self.getMsg(name=author, url=rep[unicode('data')][unicode('image_url')]))
 
 if __name__ == '__main__':
