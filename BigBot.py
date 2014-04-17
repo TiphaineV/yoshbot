8d32c6df613100b293e2ea3a282aa3b436b78ca7
ksad <noe.gaumont@gmail.com>
2014-04-17 16:17:42 +0200

    Add random sentence feature

--- BigBot.py
+++ BigBot.py
@@ -4,17 +4,25 @@ import pdb
 import urllib2
 import json
 import random
+from string import Template
 
 sentences = []
 
 class BigBot(ircbot.SingleServerIRCBot):
 	def __init__(self):
 		ircbot.SingleServerIRCBot.__init__(self, [('jordanviard.com', 6667)], 'BigBot', 'Bot posteur de GIFs')
+		self.sentences= [ "Petit coquin, va : $url",
+				"Un gif plein d'amour pour toi, $name : $url",
+				"Cadeau : $url",
+				"Non tu n'en aura pas $name!" ]
 	
 	def on_welcome(self, serv, ev):
 		print 'Joining #Yoshteam'
 		serv.join('#YoshTeam')
 	
+	def getMsg(self, **arg):
+		return Template(random.choice(self.sentences)).safe_substitute(arg)
+
 	def on_pubmsg(self, serv, ev):
 		author = irclib.nm_to_n(ev.source())
 		channel = ev.target()
@@ -23,7 +31,7 @@ class BigBot(ircbot.SingleServerIRCBot):
 		if message.find('gif') is not -1:
 			json_resp = urllib2.urlopen('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC').read()
 			rep = json.loads(json_resp)
-			serv.privmsg(channel, "Un gif plein d'amour pour toi, " + author + " : " + rep[unicode('data')][unicode('image_url')])
+			serv.privmsg(channel,getMsg(name=author, url=rep[unicode('data')][unicode('image_url')]))
 
 if __name__ == '__main__':
 	print 'Bot running...'
