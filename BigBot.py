47bca121c9d38e1234f10f994282bf0c1dbac182
Tiphaine Viard <tiphaine.viard@gmail.com>
2014-04-18 09:35:54 +0200

    Added code from ksadorf : selection from many sources

--- BigBot.py
+++ BigBot.py
@@ -5,11 +5,13 @@ import urllib2
 import json
 import random
 from string import Template
+import re
 
 class BigBot(ircbot.SingleServerIRCBot):
 	def __init__(self):
 		ircbot.SingleServerIRCBot.__init__(self, [('jordanviard.com', 6667)], 'BigBot', 'Bot posteur de GIFs')
-		self.sentences= [ "Petit coquin, va : $url",
+		self.sources = ["giphy","twitter"]
+		self.sentences = [ "Petit coquin, va : $url",
 				"Un gif plein d'amour pour toi, $name : $url",
 				"Cadeau : $url",
 				"Non tu n'en aura pas $name!" ]
@@ -21,31 +23,46 @@ class BigBot(ircbot.SingleServerIRCBot):
 	def getMsg(self, **arg):
 		return Template(random.choice(self.sentences)).safe_substitute(arg)
 
-	def getGif(self, message):
-		tags = ""
-
-		if message[0:4].lower()  == '!gif':
-			contents = message.split(" ")
-			cats = contents[1:]
-			tags = '+'.join(cats)
-
-		json_resp = urllib2.urlopen('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + str(tags)).read()
-                rep = json.loads(json_resp)
+	def getGiphy(self,tags):
+		tab = tags.split(" ")
+		strTag='+'.join(tab)
+		json_resp = urllib2.urlopen('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + str(strTag)).read()
+        	rep = json.loads(json_resp)
 		if len(rep[unicode('data')]) is 0:
 			rep[unicode('data')] = {}
 			rep[unicode('data')][unicode('image_url')] = "i__i"
 
 		return rep
 
+	def getGif(self, message):
+		tags = []
+		m = re.search('(?<=\!)(\w+)(.*)', message)
+		
+		if (m is not None):
+			source=m.group(1)
+			tags=m.group(2)
+		else:
+			source=random.choice(self.sources)
+			tags=''
+
+		if(source == "giphy"):
+			print('giphy')
+			return self.getGiphy(tags)
+		elif(source == "twitter"):
+			print("twitter")
+			return self.getGiphy(tags)
+		
+		print("non reconnue")
+
 	def on_pubmsg(self, serv, ev):
 		author = irclib.nm_to_n(ev.source())
 		channel = ev.target()
 		message = ev.arguments()[0].lower()
 		
-		if message.find('gif') is not -1:
+		if message.find('gif') is not -1 or message.find('giphy') is not -1 or message.find('twitter') is not -1:
 			rep = self.getGif(message)
 			serv.privmsg(channel,self.getMsg(name=author, url=rep[unicode('data')][unicode('image_url')]))
-
+		
 if __name__ == '__main__':
 	print 'Bot running...'
 	B = BigBot().start()
